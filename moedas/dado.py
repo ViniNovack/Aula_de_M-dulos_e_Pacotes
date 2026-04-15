2971e7146f2dfa2b14589a1f75b50eb2a809db5b
ViniNovack <vininomn@gmail.com>
2026-04-15 08:09:15 -0300

    Atualização exercicio 110

--- moedas/dado.py
+++ moedas/dado.py
@@ -2,33 +2,16 @@ def leiaDinheiro(texto):
     while True:
         n = str(input(texto)).strip()
         if ',' in n:
-            n = n.replace(',','1')
+            n = n.replace(',','.')
         
-        if n.isnumeric():
-            break
-        else:
-            print(f'ERRO: {n} é inválido!')
+        try:
+            if '.' in n:
+                n = float(n)
+                break
+            else:
+                n = int(n)
+                break
+        except:
+            print(f'ERRO: "{n}" é invalido.')
             continue
-    n = n.replace('1', '.')
-    if '.' in n:
-        n = float(n)
-    else:
-        n = int(n)
     return n
-
-x = leiaDinheiro('Digite: ')
-print(x)
-
-
-
-def leiaDinheiro(texto):
-    while True:
-        # Lemos a entrada, limpamos espaços e trocamos a vírgula pelo ponto
-        entrada = str(input(texto)).strip().replace(',', '.')
-
-        # Validamos se a string resultante pode ser convertida para float
-        if entrada.replace('.', '', 1).isdigit() and entrada != '':
-            return float(entrada)
-        else:
-            print(f'\033[0;31mERRO: "{entrada}" é um preço inválido!\033[m')
-
